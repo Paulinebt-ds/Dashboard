7aab0f84a70a12c240c829a4ad0f905063c8e739
Paulinebt-ds <85350007+Paulinebt-ds@users.noreply.github.com>
2022-01-05 18:17:38 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -6,14 +6,42 @@ from dash.dependencies import Input, Output
 import dash_labs as dl  # pip install dash-labs
 from ressources.app import app, server
 from components.pages_plugin import *
-
+from components.functions import navbarcurrentpage, corporate_colors
 from pages.layout_client import data_domain
 
 
 df = data_domain[data_domain["SK_ID_CURR"] == int(100002)]
 df = df.to_dict('records')
 # layout rendu par l'application
-navbar = dbc.NavbarSimple(
+header = html.Div([
+
+        html.Div([
+            html.H1(children='Dashboard',
+                    style={'textAlign': 'center',
+                           'color': 'white'})
+                ],
+                className='col-8',
+                style={'padding-top': '1%'}
+                ),
+
+        html.Div([
+            html.Img(
+                    src=app.get_asset_url("logo_pret_a_depenser.PNG"),
+                    height='100',
+                    width='200')
+            ],
+            className='col-2',
+            style={
+                    'align-items': 'center',
+                    'padding-top': '1%',
+                    'height': '100'})
+
+        ],
+        className='row',
+        style={'height': '4%',
+               'background-color': 'rgb(41, 56, 55)'}
+        )
+old_navbar = dbc.NavbarSimple(
     dbc.DropdownMenu(
         [
             dbc.DropdownMenuItem(page["name"], href=page["path"])
@@ -29,11 +57,32 @@ navbar = dbc.NavbarSimple(
     className="mb-2",
 )
 
+navbar = html.Div([
+
+        html.Div([dbc.Nav(
+            [
+                dbc.NavItem(
+                    dbc.NavLink(page["name"], href=page["path"],  style=navbarcurrentpage)
+                    for page in dash.page_registry.values())
+
+            ],
+            fill=True,
+            justified=True
+        )
+            ],
+            className='col-3'),
+
+    ],
+    className='row',
+    style={'background-color': corporate_colors['dark-green'],
+           'box-shadow': '2px 5px 5px 1px rgba(255, 101, 131, .5)'}
+    )
+
 app.layout = dbc.Container(
-    [navbar,
+    [header,
+     old_navbar,
      dcc.Store(id="memory-output", data=df),
-     dl.plugins.page_container],
-    fluid=True,
+     dl.plugins.page_container]
 )
 
 
